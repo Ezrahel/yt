aa112f5194eb4bd4061c4f5a5d4a4c12673b5bd0
ezrahel <adelakinisrael024@gmail.com>
2023-10-15 17:01:56 +0100

    docker file

--- account/main.go
+++ account/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	log.Println("Starting server... ")
-	router := gin.Default
+	router := gin.Default()
+	router.GET("/", func(ctx *gin.Context) {
+		c.JSON(http.StatusOK, gin.H[
+			"Hello": "Ajay"
+		])
+	})
+	svc:=&http.Serve{
+		addr: "8080",
+		Handler: router,
+	}
 }
